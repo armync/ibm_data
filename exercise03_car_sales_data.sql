a038245da71f0529a7c63ca8e3b5acc35cad10c1
ArminC <arminandrey@gmail.com>
2025-02-14 13:33:41 +0200

    ibm_data

---
+++ exercise03_car_sales_data.sql
@@ -0,0 +1,7 @@
+SELECT * FROM exercise03_car_sales_data;
+
+SELECT COUNT(*) FROM exercise03_car_sales_data;
+
+select max(price) as max_price from exercise03_car_sales_data;
+
+select distinct(model) from exercise03_car_sales_data;
---
+++ unit_test/mymodule.py
@@ -0,0 +1,31 @@
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a,b):
+    """
+    This function returns the sum of the given numbers
+    """
+    return a + b
+
+'''
+def add(a,b):
+    """
+    This function returns the sum of the given numbers
+    """
+    return a + b
+'''
+
+def add(a,b): # purposefully broken
+    """
+    This function returns the sum of the given numbers
+    """
+    return a + b + 1
